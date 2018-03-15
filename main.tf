b46cbf668c68fb73113c0b6e71638ed86d2a956f
rightlag <rightlag@gmail.com>
2018-03-15 15:39:34 -0400

    Add debugging options

--- main.tf
+++ main.tf
@@ -1,6 +1,11 @@
 provider "vsphere" {
+  // Provider options
   allow_unverified_ssl = "${var.allow_unverified_ssl}"
   password             = "${var.password}"
   user                 = "${var.user}"
   vsphere_server       = "${var.vsphere_server}"
+
+  // Debugging options
+  client_debug      = "${var.client_debug}"
+  client_debug_path = "${var.client_debug_path}"
 }
--- variables.tf
+++ variables.tf
@@ -1,3 +1,5 @@
+// Provider options
+
 variable "allow_unverified_ssl" {
   default     = false
   description = "Disable SSL certificate verification"
@@ -13,4 +15,16 @@ variable "user" {
 
 variable "vsphere_server" {
   description = "vCenter server name for vSphere API operations"
+}
+
+// Debugging options
+
+variable "client_debug" {
+  default     = false
+  description = "Log SOAP calls made to the vSphere API to disk"
+}
+
+variable "client_debug_path" {
+  default     = "${pathexpand("~")/.govmomi}"
+  description = "Default log path"
 }
